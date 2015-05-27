9c2f85834e2874fc0861a540eb2aa54ff183d3c0
sergio <alenios@hotmail.com>
2015-05-27 17:24:01 -0430

    consultar saldo funcional con acceso a billetera para admin

--- estacionamientos/admin.py
+++ estacionamientos/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from estacionamientos.models import Estacionamiento, Reserva, Pago, TarifaMinuto,\
-    TarifaHorayFraccion, TarifaHora
+    TarifaHorayFraccion, TarifaHora, BilleteraElectronica
 
 admin.site.register(Estacionamiento)
 admin.site.register(Reserva)
@@ -9,3 +9,4 @@ admin.site.register(Pago)
 admin.site.register(TarifaHora)
 admin.site.register(TarifaMinuto)
 admin.site.register(TarifaHorayFraccion)
+admin.site.register(BilleteraElectronica)
