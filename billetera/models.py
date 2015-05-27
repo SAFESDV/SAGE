e521cde09532f79866b0461a024948c36790c343
debia <debia@WIN-QROB58RKPU1>
2015-05-27 00:52:36 -0430

    Wierd stuff 0.1

---
+++ billetera/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from math import ceil, floor
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from decimal import Decimal
+from datetime import timedelta
+
+class BilleteraElectronica(models.Model):
+    nombreUsuario    = models.CharField(max_length = 50)
+    apellidoUsuario  = models.CharField(max_length = 50)
+    cedulaTipo       = models.CharField(max_length = 1)
+    cedula           = models.CharField(max_length = 10)
+    PIN              = models.CharField(max_length = 4)
+    saldo            = models.DecimalField(max_digits = 5, decimal_places = 2)
+    
+    def __str__(self):
+        return str(self.id)+" "+ str(self.cedula) 
